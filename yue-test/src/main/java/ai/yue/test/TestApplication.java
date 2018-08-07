3f954c12ffca5d93da7427bfff586ec07bec2831
Shinelon <Shinelon@1>
2018-08-07 14:12:52 +0800

    1. new

---
+++ yue-test/src/main/java/ai/yue/test/TestApplication.java
@@ -0,0 +1,17 @@
+package ai.yue.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author  孙金川
+ * @version 创建时间：2018年8月7日
+ */
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(TestApplication.class, args);
+	}
+	
+}
