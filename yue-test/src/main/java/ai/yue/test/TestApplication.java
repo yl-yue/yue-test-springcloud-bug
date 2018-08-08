2e428e231b07b622e64f20c786cfd87bbc568c5b
Shinelon <Shinelon@1>
2018-08-08 11:01:46 +0800

    1. Simpler examples

--- yue-test/src/main/java/ai/yue/test/TestApplication.java
+++ yue-test/src/main/java/ai/yue/test/TestApplication.java
@@ -2,16 +2,27 @@ package ai.yue.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author  孙金川
  * @version 创建时间：2018年8月7日
  */
+@RestController
 @SpringBootApplication
 public class TestApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TestApplication.class, args);
+	}
+	
+	@GetMapping("/get")
+	public String get() {
+		String str = "Hello, world.";
+		String str2 = "World, hello.";
+		System.out.println(str);
+		return str2;
 	}
 	
 }
